35544650f3a74e16bc62e0b659b6f407138351af
JossguerreroP <josguerrerop@udistrital.edu.co>
2025-08-26 20:42:21 -0500

    Done

--- src/main/java/com/json/app/services/JsonImp.java
+++ src/main/java/com/json/app/services/JsonImp.java
@@ -1,13 +1,24 @@
 package com.json.app.services;
-
+import org.springframework.stereotype.Service;
 import com.json.app.domain.MovieEntity;
+import com.json.app.repository.MovieRepository;
 
+import lombok.RequiredArgsConstructor;
+
+
+
+
+@RequiredArgsConstructor
+@Service
 public class JsonImp implements JsonService{
-
+	
+	
+	private final MovieRepository MovieRepository;
+	
 	@Override
 	public MovieEntity newMovie(MovieEntity movie) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return MovieRepository.save(movie);
 	}
 
 }
